099913575586a103cd642cde7ab56b73cfe630f8
Marcos Cannabrava <mpcannabrava@gmail.com>
2021-05-07 12:21:28 -0500

    update twitter url

--- src/components/social-media-list.tsx
+++ src/components/social-media-list.tsx
@@ -9,7 +9,7 @@ const SocialMediaList = () => {
   const socialMedia = [
     { title: `Instagram`, href: "https://www.instagram.com/marcoscannabrava/", file: "instagram.png" },
     { title: `GitHub`, href: "https://github.com/marcoscannabrava", file: "github3.png" },
-    { title: `Twitter`, href: "https://twitter.com/mpcannabrava", file: "twitter2.png" },
+    { title: `Twitter`, href: "https://twitter.com/cannabrav", file: "twitter2.png" },
     { title: `LinkedIn`, href: "https://www.linkedin.com/in/marcos-cannabrava/", file: "linkedin.png" },
     { title: `Email`, href: "mailto:mpcannabrava@gmail.com", file: "email.png" }
   ]
